6a0e7f68ab52bd7feace8ffc48fc0d7aaa7937a1
david plotkin <davidplo4545@gmail.com>
2023-06-28 10:43:13 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/OS4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS4.dir/main.cpp.o"
+  "CMakeFiles/OS4.dir/main.cpp.o.d"
+  "OS4"
+  "OS4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
